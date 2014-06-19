603f2b12d3881a2268adb8c4c9d00fe541648923
EricRho <eric.rho.87@gmail.com>
2014-06-18 23:21:34 -0400

    Insert rows into payment table

--- bank_homework.sql
+++ bank_homework.sql
@@ -17,15 +17,37 @@ id SERIAL PRIMARY KEY
 --  recipient_name, and description with the appropriate datatypes
 , recipient_name TEXT
 , description TEXT
-)
+);
 
 -- Alter the payments table, add the column of `zipcode`
 -- to specify where the transaction was made
-
+ALTER TABLE payments ADD zipcode;
 
 -- Insert 20 rows into the payments table
 -- The first of these payments should be deposits
 -- try to keep the accounts positive and have at least three different payers.
+INSERT INTO payments(created_at, amount, payer_name, recipient_name, description) VALUES
+ ('2014-01-01', 10000, 'Eric', 'Ian', 'Repayment for car loan')
+,('2014-05-19', 20000, 'Euan', 'Eric', 'Rental payment')
+,('2014-01-01', 15000, 'Ian', 'Euan', 'Purchace of super banana')
+,('2014-01-01', 500, 'Eric', 'Ian', 'Birthday present')
+,('2014-01-01', 700, 'Euan', 'Eric', 'Borrow for fun')
+,('2014-01-01', 10, 'Ian', 'Euan', 'Lunch')
+,('2014-01-01', 350, 'Eric', 'Ian', 'Lost a bet')
+,('2014-01-01', 400, 'Euan', 'Eric', 'Monthly allowance')
+,('2014-01-01', 200, 'Ian', 'Euan', 'Payback for plane ticket')
+,('2014-01-01', 100, 'Eric', 'Ian', 'Repayment for dinner bill')
+,('2014-01-01', 50, 'Euan', 'Eric', 'Stole money')
+,('2014-01-01', 75, 'Ian', 'Euan', 'Gave 1 dollar for every jumpingjack')
+,('2014-01-01', 30, 'Eric', 'Ian', 'Running out of ideas so gave money')
+,('2014-01-01', 120, 'Euan', 'Eric', 'Payback for new coffee maker')
+,('2014-01-01', 15, 'Ian', 'Euan', 'Gas bill')
+,('2014-01-01', 50, 'Eric', 'Ian', 'Payment for internet bill')
+,('2014-01-01', 110, 'Euan', 'Eric', 'Found and gave for no reason')
+,('2014-01-01', 700, 'Ian', 'Euan', 'Gave for hotel stay')
+,('2014-01-01', 250, 'Eric', 'Ian', 'Rental payment')
+,('2014-01-01', 70, 'Euan', 'Eric', 'Repayment for car loan');
+
 
 
 -- You receive notice that the payment with id of 9 is $10 too low.
